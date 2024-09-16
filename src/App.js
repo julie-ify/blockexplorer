1e86e529366f99f4a0c450d8cb2588a608d4bdb2
Juliana Ifionu <julieifionu@gmail.com>
2024-09-16 11:35:54 -0400

    Implement router for SPA

--- src/App.js
+++ src/App.js
@@ -1,36 +1,19 @@
-import { Alchemy, Network } from 'alchemy-sdk';
-import { useEffect, useState } from 'react';
-
+import { Route, Switch } from 'react-router-dom';
+import BlockList from './BlockList';
+import Block from './Block';
+import Transaction from './Transaction';
 import './App.css';
 
-// Refer to the README doc for more information about using API
-// keys in client-side code. You should never do this in production
-// level code.
-const settings = {
-  apiKey: process.env.REACT_APP_ALCHEMY_API_KEY,
-  network: Network.ETH_MAINNET,
-};
-
-
-// In this week's lessons we used ethers.js. Here we are using the
-// Alchemy SDK is an umbrella library with several different packages.
-//
-// You can read more about the packages here:
-//   https://docs.alchemy.com/reference/alchemy-sdk-api-surface-overview#api-surface
-const alchemy = new Alchemy(settings);
-
 function App() {
-  const [blockNumber, setBlockNumber] = useState();
-
-  useEffect(() => {
-    async function getBlockNumber() {
-      setBlockNumber(await alchemy.core.getBlockNumber());
-    }
-
-    getBlockNumber();
-  });
-
-  return <div className="App">Block Number: {blockNumber}</div>;
+	return (
+		<main className="container">
+			<Switch>
+				<Route exact path="/" component={BlockList} />
+				<Route path="/block/:block" component={Block} />
+				<Route path="/tx/:hash" component={Transaction} />
+			</Switch>
+		</main>
+	);
 }
 
 export default App;
